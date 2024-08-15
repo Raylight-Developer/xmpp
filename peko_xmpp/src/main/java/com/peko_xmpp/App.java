6ff77e80ce8508ee1f2395b702641bdcbedc731c
Raylight-Developer <Raylight.creator@gmail.com>
2024-08-14 22:26:04 -0600

    Dependencies

--- peko_xmpp/src/main/java/com/peko_xmpp/App.java
+++ peko_xmpp/src/main/java/com/peko_xmpp/App.java
@@ -1,38 +1,33 @@
 package com.peko_xmpp;
 
+// Java FX
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+// XMPP Lib
+import org.jxmpp.jid.parts.*;
+import org.jxmpp.jid.impl.*;
+import org.jxmpp.jid.*;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		StackPane root = new StackPane();
+		
+		scene = new Scene(root, 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
